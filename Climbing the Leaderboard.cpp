4c89bb77326cf6bfc2ef6ffdd3ce59c2f84e8af3
Lalit Tyagi <luckytyagi61@gmail.com>
2017-08-30 00:38:27 +0530

    Update Climbing the Leaderboard.cpp

--- Climbing the Leaderboard.cpp
+++ Climbing the Leaderboard.cpp
@@ -3,55 +3,57 @@ using namespace std;
 
 int main()
 {
-	long long int n,m,k=0,l=1,i,j;
+	int n,m;
 	cin>>n;
-	int arr[n];
-	for(i=0;i<n;i++)
-	{
+	long arr[n];
+	long arrR[n];
+	for(long i=0;i<n;i++)
 		cin>>arr[i];
-	}
 	
-	cin>>m;
-	int arr1[m];
-	int arr3[m];
-	for(j=0;j<m;j++)
+	arrR[0]=1;
+	for(long i=1;i<n;i++)
 	{
-		cin>>arr1[j];
+		if(arr[i]<arr[i-1])
+			arrR[i]=arrR[i-1]+1;
+		else
+			arrR[i]=arrR[i-1];
 	}
 	
-	int arr2[n];
-	arr2[0]=arr[0];
-	
-	for(i=1;i<n;i++)
-	{
-		if(arr[i]!=arr[i-1])
-		{
-			arr2[l]=arr[i];
-			l++;
-		}
-	}
-	
-	for(i=m-1;i>=0;i--)
-		for(j=k;j<l;j++)
-		{
-			if(arr2[j]<=arr1[i])
-			{
-				//cout<<(j+1)<<"\n";
-				arr3[i]=j+1;
-				k=j;
-				break;
-			}				
-		}
+//	for(long i=0;i<n;i++)
+//		cout<<arrR[i];
 		
-		if(arr2[l-1]>arr1[0])
+	cin>>m;
+	long rank[m];
+	long k=0;
+	for(long i=0;i<m;i++)
+	{	
+		long score;
+		cin>>score;
+		if(score>arr[0])
+		{
+			rank[k]=1;
+			k++;	
+		}
+		else
+			for(long i=n-1;i>=0;i--)
 			{
-				//cout<<(l+1);
-				arr3[0]=l+1;
-			}
-			
-		for(int i=0;i<m;i++)
-		cout<<arr3[i]<<"\n";	
+				if(score<arr[i])
+				{
+					rank[k]=arrR[i] + 1;
+					k++;
+					break;
+				}
+				else if(score==arr[i])
+				{
+					rank[k]=arrR[i];
+					k++;
+					break;
+				}		
+			}	
+	}
 	
-	
+	for(long i=0;i<k;i++)
+		cout<<rank[i]<<"\n";
+		
 	return 0;
 }
